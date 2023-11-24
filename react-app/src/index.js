532cc25d9b3fc15d67e05b0d9abb1d0bf267876a
Salima <asanbaevasalima24@mail.com>
2023-11-25 03:15:58 +0600

    react-app1

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  React.createElement('button', {
-    onClick:() => console.log('CLICK')
-  }, 'click on me'),
-  document.getElementById('root')
+  <App/>
 );
 
 reportWebVitals();
