553988af5b8b9964cb7343979e2bd482f99e5900
Salima <asanbaevasalima24@mail.com>
2023-11-26 18:31:40 +0600

    react-app3

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react';
 
 /* Local dependencies */
 import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
   const [value, setValue] = useState('input text'); 
   
   return (
     <div className="App">
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
