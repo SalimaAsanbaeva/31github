b56e7fee3af02fc9e948e1d28dfd27715cbdc00c
Salima <asanbaevasalima24@mail.com>
2023-11-27 17:45:47 +0600

    react-app7

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,12 @@
 /* External dependencies */
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 /* Local dependencies */
 import PostItem from './components/PostItem';
 import './styles/App.css';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,13 +14,32 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]); 
+  const [title, setTitle] = useState('')
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(title)
+    console.log(bodyInputRef.current.value)
+  }
   
   return (
     <div className="App">
       <form>
-        <input type="text" placeholder="Post title"/>
-        <input type="text" placeholder="Post description"/>
-        <button>Create a post</button>
+        { /*Controlled component*/}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+             type="text" 
+             placeholder="Post title"
+        />
+        <input ref={ bodyInputRef} type="text" />
+        {/* <MyInput
+           ref={bodyInputRef}
+           type="text" 
+           placeholder="Post description"
+        /> */}
+        <MyButton onClick={ addNewPost } >Create a post</MyButton>
       </form>
       <PostList posts={posts} title="List of posts" />
     </div>
