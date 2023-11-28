94e6da647bec571e8fe1b45b83b4cdfb07b67fd1
Salima <asanbaevasalima24@mail.com>
2023-11-28 18:24:44 +0600

    react-app9

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,8 +5,7 @@ import React, { useState, useRef } from 'react';
 import PostItem from './components/PostItem';
 import './styles/App.css';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -14,33 +13,14 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]); 
-  const [post, setPost] = useState({title: '', body: ''});
-  
 
-  const addNewPost = (e) => {
-    e.preventDefault()
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''})
+  const createPost = (newPost) => {
+      setPosts([...posts, newPost])
   }
   
   return (
     <div className="App">
-      <form>
-        { /*Controlled component*/}
-        <MyInput
-             value={post.title}
-             onChange={e => setPost({...post, title: e.target.value})}
-             type="text" 
-             placeholder="Post title"
-        />
-        <MyInput
-           value={post.body}
-           onChange={e => setPost({...post, body: e.target.value})}
-           type="text" 
-           placeholder="Post description"
-        />
-        <MyButton onClick={ addNewPost } >Create a post</MyButton>
-      </form>
+      <PostForm create={ createPost} />
       <PostList posts={posts} title="List of posts" />
     </div>
   );
---
+++ react-app/src/components/PostForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+
+function PostForm({create}) {
+    const [post, setPost] = useState({ title: "", body: "" });
+
+   const addNewPost = (e) => {
+     e.preventDefault();
+     const newPost = {
+       ...post, id: Date.now()
+     }
+     create(newPost)
+     setPost({ title: "", body: "" });
+   };
+  
+  return (
+    <form>
+      {/*Controlled component*/}
+      <MyInput
+        value={post.title}
+        onChange={(e) => setPost({ ...post, title: e.target.value })}
+        type="text"
+        placeholder="Post title"
+      />
+      <MyInput
+        value={post.body}
+        onChange={(e) => setPost({ ...post, body: e.target.value })}
+        type="text"
+        placeholder="Post description"
+      />
+      <MyButton onClick={addNewPost}>Create a post</MyButton>
+    </form>
+  );
+}
+
+export default PostForm
