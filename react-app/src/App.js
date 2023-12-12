5fe99307cceb40dc3194fe5186458382bc18da40
Salima <asanbaevasalima24@mail.com>
2023-12-12 17:48:33 +0600

    react-app29

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,31 +8,13 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "аа", body: "бб" },
-    { id: 2, title: "гг", body: "аа" },
-    { id: 3, title: "вв", body: "яя" },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    console.log("Sorted posts function worked");
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query)
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
